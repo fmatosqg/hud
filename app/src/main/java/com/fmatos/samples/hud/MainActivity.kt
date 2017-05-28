d74c783cfb745788ad0d793f0886242f7eb55b86
Fabio de Matos <fmatos@monits.com>
2017-05-28 20:03:03 -0300

    Add more info

--- app/src/main/java/com/fmatos/samples/hud/MainActivity.kt
+++ app/src/main/java/com/fmatos/samples/hud/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.fmatos.samples.hud
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.text.format.Formatter
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import org.joda.time.LocalDate
+import org.joda.time.LocalTime
+
 
 class MainActivity : AppCompatActivity() {
 
     private var TAG: String = MainActivity::class.java.simpleName
 
     private var handler: Handler? = null
-    private var text: String = ""
+    private var time: String = ""
+    private var date: String = ""
+    private var ip: String = ""
     private var textBlink: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +29,6 @@ class MainActivity : AppCompatActivity() {
 
         handler = Handler(mainLooper)
 
-        text = " 8:11"
         updateScreen()
     }
 
@@ -29,19 +36,46 @@ class MainActivity : AppCompatActivity() {
 
         handler!!.postDelayed(this::updateScreen, 1000)
 
+        updateModel()
         textBlink = !textBlink
 
         var textOutput: String
 
-        textOutput = text.replace(' ', '!')
+        textOutput = time.replace(' ', '!')
 
         if (textBlink) {
             textOutput = textOutput.replace(':', ' ')
         }
 
-        clock_text.text = textOutput
+        clock_time_text.text = textOutput
+        clock_date_text.text = date
+        ip_text.text = ip
 
         Log.d(TAG,"Message = " + textOutput)
+    }
+
+    private fun updateModel() {
+
+        var localTime = LocalTime.now()
+        time = localTime.toString("HH:mm")
+
+        var localDate = LocalDate.now()
+        date = localDate.toString("dd/MM/yyyy")
+
+        getWifiIp()
+    }
+
+    private fun getWifiIp() {
+        val wifiMgr = getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val wifiInfo = wifiMgr.connectionInfo
+        ip = wifiInfo.ssid
+
+        ip += "\n"
+        ip += Formatter.formatIpAddress(wifiInfo.ipAddress)
+
+//        var host = InetAddress.getLocalHost() // uses network code, can't call on main thread
+//        ip = host.hostAddress
+
     }
 }
 
