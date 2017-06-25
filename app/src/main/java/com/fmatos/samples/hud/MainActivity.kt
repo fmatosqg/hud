1107fae729094f3b6112ef297a4f780bbdfa8b9b
Fabio de Matos <fmatos@monits.com>
2017-06-25 13:08:07 +1000

    Convert looper and handler to rx

--- app/src/main/java/com/fmatos/samples/hud/MainActivity.kt
+++ app/src/main/java/com/fmatos/samples/hud/MainActivity.kt
@@ -3,15 +3,20 @@ package com.fmatos.samples.hud
 import android.content.Context
 import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.text.format.Formatter
 import android.util.Log
 import com.bumptech.glide.Glide
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.Timed
 import kotlinx.android.synthetic.main.activity_main.*
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
 import org.joda.time.format.DateTimeFormat
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,19 +24,18 @@ class MainActivity : AppCompatActivity() {
     private var TAG: String = MainActivity::class.java.simpleName
     private val INTERVAL_1_SECOND_MS: Long = 1000
 
-    private var handler: Handler? = null
     private var time: String = ""
     private var date: String = ""
     private var ip: String = ""
     private var textBlink: Boolean = true
     private var test: String = ""
 
+    private val disposables = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        handler = Handler(mainLooper)
 
         Glide.with(this)
                 .load("https://s-media-cache-ak0.pinimg.com/originals/28/47/ed/2847edeab41b3d90a849c68340b5be3a.jpg")
@@ -39,14 +43,27 @@ class MainActivity : AppCompatActivity() {
                 .error(R.drawable.rocket_diamonds)
                 .into(background_img);
 
-        updateScreen()
+        var clock1Sec = Observable.interval(INTERVAL_1_SECOND_MS, TimeUnit.MILLISECONDS)
+                .timeInterval()
+                .subscribeOn(Schedulers.computation())
+
+        clock1Sec
+                .observeOn(Schedulers.computation())
+                .subscribe({ longTimed -> updateModel(longTimed) })
+
+        clock1Sec
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ updateScreen() })
+
+    }
+
+    override fun onDestroy() {
+        disposables.clear()
+        super.onDestroy()
     }
 
     private fun updateScreen() {
 
-        handler!!.postDelayed(this::updateScreen, INTERVAL_1_SECOND_MS)
-
-        updateModel()
         textBlink = !textBlink
 
         var timeView: String = time
@@ -68,7 +85,7 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "Message = " + timeView)
     }
 
-    private fun updateModel() {
+    private fun updateModel(longTimed: Timed<Long>) {
 
 //        val dateTime = DateTime.now().toDateTime(DateTimeZone.forOffsetHours(-3))
 //        val locale = java.util.Locale("es", "AR")
@@ -79,7 +96,7 @@ class MainActivity : AppCompatActivity() {
         time = DateTimeFormat.forPattern("h:mm").withLocale(locale).print(dateTime)
         date = DateTimeFormat.forPattern("EEEEE, dd MMMM yyyy").withLocale(locale).print(dateTime)
 
-        test = "testt"
+        test = "${longTimed.value()}"
         getWifiIp()
     }
 
--- app/src/main/java/com/fmatos/samples/hud/sample/Sample.java
+++ app/src/main/java/com/fmatos/samples/hud/sample/Sample.java
@@ -1,5 +1,8 @@
 package com.fmatos.samples.hud.sample;
 
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Timed;
+
 /**
  * Created by fmatos on 28/05/2017.
  */
@@ -8,5 +11,22 @@ public class Sample {
 
     private void useless() {
 
+        new DisposableObserver<Timed<Long>>(){
+
+            @Override
+            public void onNext(Timed<Long> longTimed) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
     }
 }
