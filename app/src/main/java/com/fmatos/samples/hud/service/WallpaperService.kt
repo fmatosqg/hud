66c6e79c9bf6531d18a114813a817ea20b7c290f
Fabio de Matos <fmatos@monits.com>
2017-07-01 16:42:22 +1000

    Add retrofit to download urls from amazing wallpapers

--- app/src/main/java/com/fmatos/samples/hud/service/WallpaperService.kt
+++ app/src/main/java/com/fmatos/samples/hud/service/WallpaperService.kt
@@ -1,13 +1,13 @@
 package com.fmatos.samples.hud.service
 
 import android.util.Log
+import com.fmatos.samples.hud.service.model.amazingwallpapers.Album
 import com.fmatos.samples.hud.service.model.amazingwallpapers.AmazingWallpapersService
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.rxkotlin.toObservable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
-import io.reactivex.subjects.ReplaySubject
+import io.reactivex.subjects.Subject
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -21,39 +21,52 @@ class WallpaperService {
 
     private var TAG: String = WallpaperService::class.java.simpleName
 
-//    http://amazingdomain.net/album/customrss?url=http://instatom.freelancis.net/scottkelby
-
-
-    private var subject: BehaviorSubject<String>? = null
-
-    private var subject2: ReplaySubject<String> = ReplaySubject.create()
-
-    val sampleList = listOf<String>(
-            "https://scontent-amt2-1.cdninstagram.com/t51.2885-15/e35/19428895_1442540705784136_5195084963979984896_n.jpg",
-            "https://scontent-amt2-1.cdninstagram.com/t51.2885-15/e35/c180.0.720.720/19228605_1940749149543021_747073109751758848_n.jpg",
-            "https://scontent-amt2-1.cdninstagram.com/t51.2885-15/e35/c180.0.720.720/18513836_220288391802329_621043874340536320_n.jpg"
-
-    )
-
     fun buildObservable(): Observable<String> {
 
-        return buildList().toObservable()
+        return buildListObservable()
                 .repeatUntil({false})
 
     }
 
-    private fun buildList(): List<String> {
+    private fun buildListObservable(): Subject<String> {
 
-        fetchData()
+        val observableAlbum = fetchData()
 
-        return sampleList
+        val subject: Subject<String> = BehaviorSubject.create()
+
+
+        observableAlbum
+                .observeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.newThread())
+                .subscribeBy(
+                        onComplete = {
+                            Log.i(TAG, "On album complete")
+                        },
+                        onNext = {
+                            album ->
+                            Log.i(TAG, "On Album name is " + album.name)
+
+                            album.photos
+                                    .filter { photo -> photo.url != null }
+                                    .map { photo ->
+                                        val url: String = photo.url ?: ""
+                                        subject.onNext(url)
+                                    }
+
+                        },
+                        onError = {
+                            Log.i(TAG, "On error " + it)
+                        }
+                )
+
+
+        return subject
     }
 
-    private fun fetchData() {
+    private fun fetchData(): Observable<Album> {
 
         val retrofit = Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//                .addConverterFactory(JacksonConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("http://amazingdomain.net/")
                 .build()
@@ -63,19 +76,7 @@ class WallpaperService {
         val amazingWallpaperService = retrofit.create(AmazingWallpapersService::class.java)
         val scottKelbyAlbum = amazingWallpaperService.getAlbum(ALBUM_INSTAGRAM_SCOTT_KELBY)
 
-
-        scottKelbyAlbum.subscribeOn(Schedulers.newThread())
-                .observeOn(Schedulers.newThread())
-                .subscribeBy(
-                        onComplete = {
-                            Log.i(TAG,"On album error")
-                        },
-                        onNext = {
-                            album -> Log.i(TAG, "On Album name is " + album.name)
-                        },
-                        onError = {
-                            Log.i(TAG,"On error " + it)
-                        }
-                )
+        return scottKelbyAlbum.subscribeOn(Schedulers.newThread())
     }
+
 }
--- app/src/main/java/com/fmatos/samples/hud/service/model/amazingwallpapers/Photo.kt
+++ app/src/main/java/com/fmatos/samples/hud/service/model/amazingwallpapers/Photo.kt
@@ -4,5 +4,4 @@ package com.fmatos.samples.hud.service.model.amazingwallpapers
  * Created by fmatos on 1/07/2017.
  */
 
-
-data class Album(val name: String?, val niceName: String?, val photos: List<Photo>)
+data class Photo(val caption: String?, val url: String?, val description: String?)
