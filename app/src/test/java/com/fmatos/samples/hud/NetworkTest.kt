10fefca5030c21b45493217171fa0931ca029bfa
Fabio de Matos <fmatosqg@gmail.com>
2017-09-15 22:15:49 +1000

    Convert build files back to gradle android plugin 2.3.3

---
+++ app/src/test/java/com/fmatos/samples/hud/NetworkTest.kt
@@ -0,0 +1,25 @@
+package com.fmatos.samples.hud
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * Created by fmatos on 15/09/2017.
+ */
+@RunWith(JUnit4::class)
+class NetworkTest {
+
+
+    @Test
+    fun testTest() {
+        assertTrue(true)
+    }
+
+    @Test
+    fun successResponseTest() {
+
+        assertTrue(true)
+    }
+}
